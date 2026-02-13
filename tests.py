e07ca358242603ae00cb4cf72091e7aa5692fe52
rpbeam04 <rpbeam04@gmail.com>
2026-02-13 17:40:36 -0600

    added fake test

--- tests.py
+++ tests.py
@@ -11,5 +11,16 @@ def test_read_simple_1():
 
     return True
 
+def test_fake_simple_1():
+    file = "Fake Data/fake_data_test_1.csv"
+    voters = read_election_data(file)
+    assert len(voters) == 6
+    for voter in voters:
+        print(voter)
+
+    return True
+
 if __name__ == "__main__":
     assert test_read_simple_1()
+    print()
+    assert test_fake_simple_1()
